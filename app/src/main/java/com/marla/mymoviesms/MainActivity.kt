bd60bd1bd09548c98e455b5461ab252e57193d4f
MarlaSantos <72895099+marlasantos24@users.noreply.github.com>
2022-05-20 19:43:40 -0500

    Text_CargarImagenes

--- app/src/main/java/com/marla/mymoviesms/MainActivity.kt
+++ app/src/main/java/com/marla/mymoviesms/MainActivity.kt
@@ -3,41 +3,176 @@ package com.marla.mymoviesms
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.PlayCircleOutline
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
-import com.marla.mymoviesms.ui.theme.MyMoviesMsTheme
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import coil.compose.rememberImagePainter
+import com.example.mymoviesHP.ui.screens.detail.DetailScreen
+import com.example.mymoviesHP.ui.theme.MyMoviesHPTheme
 
+@ExperimentalFoundationApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyMoviesMsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("AndroidMs")
+            val navController = rememberNavController()
+            NavHost(navController = navController, startDestination = "main"){
+                composable("main"){
+                    MyMoviesApp {
+                        MainScreen(navController)
+                    }
+                }
+                composable(route ="detail/{mediaId}", arguments =listOf(navArgument("mediaId"){type = NavType.IntType})
+                ){backStackEntry ->
+                    val id = backStackEntry.arguments?.getInt("mediaId")
+                    requireNotNull(id)
+                    DetailScreen(id)
                 }
             }
+            /*MyMoviesApp {
+                MainScreen()
+                }*/
         }
     }
+
+
 }
 
+
+
+
+
+
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(text = "Hello $name!", modifier = modifier)
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true, widthDp = 200, heightDp = 100)
 @Composable
-fun DefaultPreview() {
-    MyMoviesMsTheme {
-        Greeting("Android")
+fun ButtonText(){
+    Box(modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ){
+        Text(
+            text = "Hello World",
+
+            //onTextLayout =
+            //lineHeight = 2.em
+        )
     }
 }
+/* CONFIGURACIONES AL TEXTO
+* color = Color.Red,
+            fontSize = 25.sp,
+            fontStyle = FontStyle.Italic,
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily.Monospace,
+            letterSpacing = 5.sp,
+            textDecoration = TextDecoration.LineThrough,
+            textAlign = TextAlign.Center,
+            maxLines = 1,
+            softWrap = false,
+            overflow = TextOverflow.Ellipsis,
+            style = MaterialTheme.typography.h4.copy(
+                shadow = Shadow(
+                    offset = Offset(5f, 5f),
+                    blurRadius = 5f,
+                    color = Color.Black.copy(alpha = 0.5f)
+                )
+            )
+* */
+
+/*MODIFIER DE BUTTONTEXT
+* modifier = Modifier
+                .clickable {
+                    /*TODO*/
+                }
+                .background(Color.Cyan)
+                .border(2.dp, Color.Blue)
+                .padding(horizontal = 16.dp, vertical = 8.dp)
+* */
+
+//@Preview(showBackground = true, name = "Android Greeting")
+@Composable
+fun DefaultPreview(){
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(200.dp),
+        horizontalArrangement = Arrangement.SpaceAround,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Greeting(
+            name = "Android",
+            modifier = Modifier.background(Color.LightGray)
+        )
+    }
+}
+
+/*
+var (value, onValueChange) = rememberSaveable{ mutableStateOf("")}
+StateSample(
+value = value,
+onValueChange = onValueChange
+)
+//@Preview(showBackground = true, widthDp = 400, heightDp = 400)
+@Composable
+fun StateSample(value:String, onValueChange:(String)->Unit){
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(64.dp),
+        verticalArrangement = Arrangement.Center
+    ){
+        TextField(
+            value = value,
+            onValueChange = {onValueChange(it)},
+            modifier = Modifier.fillMaxWidth()
+        )
+        Text(
+            text = value,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Yellow)
+                .padding(8.dp)
+        )
+        Button(
+            onClick = { onValueChange("") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = value.isNotEmpty()
+        ) {
+            Text(text = "Clear")
+        }
+    }
+}*/
---
+++ app/src/main/java/com/marla/mymoviesms/ui/screens/main/MainScreen.kt
@@ -0,0 +1,2 @@
+package com.marla.mymoviesms.ui.screens.main
+
